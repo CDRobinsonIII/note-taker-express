a861f36528ae561292dccbe7959134d71e77d57f
Charles Robinson <the.real.robinsons@gmail.com>
2020-12-17 15:00:14 -0800

    Updated server.js file and added html routes to htmlRoutes.js file.

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = function(app) {
+    
+    app.get("/", function (request, response) {
+        response.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    app.get("/notes", function (request, response) {
+        response.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    app.get("*", function(request, response) {
+        response.sendFile(path.join(__dirname, "../public/index.html"));
+      });
+}
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const express = require("express");
 
+// Tells node that we are creating an "express" server
 const app = express();
 
-app.listen(3000, function () {
-    console.log('Listening on port 3000')
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// The below points our server to a series of "route" files.
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
