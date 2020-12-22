6ac5857a92f46b44e690ebb80425615e7fbadbf2
Charles Robinson <the.real.robinsons@gmail.com>
2020-12-21 18:38:39 -0800

    Added README.md file for application.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # note-taker-express
+![GitHub License](https://img.shields.io/badge/License-MIT-blue) 
+
+## Description 
+The mission of this project was to build a note taker application using Node Express server. The user can add new notes, save them, review saved notes, and delete saved notes. 
+
+## Table of Contents
+* [Technologies Used](#technologies-used)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Technologies Used
+<ul>
+   <li>JavaScript</li> <li>Node JS</li> <li>Express JS Server</li> <li>Heroku</li>
+</ul>
+
+## Installation
+To install the note-taker-express application you will have to fork the repository and then clone the repository to your local machine. Once you CD into the directory of the team-generator-CLI-application you will need to run the following command to install the dependencies needed to run the application: 
+```
+NPM install 
+```
+
+## Usage 
+To run the note-taker-express application you can run it via the deployed heroku link: https://note-taker-express-cdr.herokuapp.com/ or after the required modules have been installed with the NPM install command, you will have to enter the following command line: 
+```
+node server.js. 
+```
+Then open the http://localhost:3000/ in your browser. 
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+If you would like to contribute to this repository and help improve it, your contributions would be welcomed. Please contact me via my contact information provided below.
+
+## Tests
+There are no built-in tests for the note-taker-express application. You can test the application by adding notes and saving them by clicking on the save button. They should appear on the left side of the application in a saved note list. You should be able to click on the delete button to removed them from the saved list. You should also be able to review any of the notes in the saved list by click on them, then they will appear on the right in the note section. 
+
+## Questions
+If you have any questions about this note-taker-express application or to report any issues with the application please email me at charlesdalerobinson@gmail.com.
+Please visit my github page to review my other repositories: [CDRobinsonIII](https://github.com/CDRobinsonIII)
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
 const notesDatabase = require("../db/db.json");
+// const path = require("path");
+// const util = require("util");
+// const readAsync = util.promisify(fs.readFile);
+// const writeAsync = util.promisify(fs.writeFile);
 
+// const getNotes = () => {
+//     return readAsync(path.join(__dirname,"..//db/db.json"), "utf8").then((notes) => {
+//         return JSON.parse(notes);
+//     })
+// }
 
 module.exports = function(app) {
 
@@ -14,7 +23,7 @@ module.exports = function(app) {
         const newObject = req.body;
         newObject.id = notesDatabase[notesDatabase.length-1].id+1;
         notesDatabase.push(req.body);
-        fs.writeFile("./db/db.json", JSON.stringify(notesDatabase), function (error) {
+        fs.writeFile("../db/db.json", JSON.stringify(notesDatabase), function (error) {
             if (error) 
                 throw error;
         })
@@ -26,14 +35,32 @@ module.exports = function(app) {
         const id = req.params.id;
         console.log("Hello they hit delete");
         console.log(id);
+        console.log("Can I read the notesDAtabase without reading it? " + notesDatabase.id);
+        fs.readFile(".//db/db.json", 'utf8', function(error, notes){ 
+            if (error) 
+            throw error;
+            // Display the file content 
+            console.log("Here are the notes: " + notes); 
+            // return notes;
+        }); 
+        console.log("Here are the notes outside of the read function: " + parse.JSON(notes)); 
 
-        for (var i=0; i < notesDatabase.length; i++) {
-            console.log("This is the id in the notesDatabase: "+notesDatabase[i].id);
-            if (JSON.stringify(notesDatabase[i].id) === id) {
-                console.log("This is the id to be deleted: " + id);
-                notesDatabase.splice(id, 1);
-                console.log("Array after delete: "+notesDatabase[1].id);
-            }
-        }
-    });
+        const notesFilteredDatabase = notes.filter(u=> {u.id != notes.id});
+        fs.writeFile(path.join(".//db/db.json"), JSON.stringify(notesFilteredDatabase), function (error) {
+            if (error) 
+                throw error;
+        })
+
+        res.json(notesFilteredDatabase);
+
+        })
+
+        // for (var i=0; i < notesDatabase.length; i++) {
+        //     console.log("This is the id in the notesDatabase: "+notesDatabase[i].id);
+        //     if (JSON.stringify(notesDatabase[i].id) === id) {
+        //         console.log("This is the id to be deleted: " + id);
+        //         notesDatabase.splice(id, 1);
+        //         console.log("Array after delete: "+notesDatabase[1].id);
+        //     }
+        // }
 }
